eab12d9b4beea2e0fea57552491f1cd661e6e60e
Mustapha Mardi <sstof818@gmail.com>
2023-12-29 19:48:59 +0100

    change intro v3

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     experimental: {
         serverActions: true,
       },
       output: 'export',
-=======
-    // experimental: {
-    //     serverActions: true,
-    //   },
-      output: 'export',
-      images: { unoptimized: true } 
-
->>>>>>> d46684f0390685ceb85960057bf890f0456c268f
 }
 
 module.exports = nextConfig
