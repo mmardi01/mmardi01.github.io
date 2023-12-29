4e3192fd6d98c3c206ff56b1059f419ced6f0c6f
Mustapha Mardi <sstof818@gmail.com>
2023-12-29 19:51:09 +0100

    update next.conf

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-      },
-      output: 'export',
+  // experimental: {
+  //     serverActions: true,
+  //   },
+    output: 'export',
+    images: { unoptimized: true } 
+
 }
 
 module.exports = nextConfig
